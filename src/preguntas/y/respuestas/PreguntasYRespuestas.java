b3d9351a4261a1347e882d9edb078bbd950a9803
Daniel Alzate <104457674+dani10x@users.noreply.github.com>
2022-04-27 10:48:14 -0500

    implementacion de las clases faltantes

--- src/preguntas/y/respuestas/PreguntasYRespuestas.java
+++ src/preguntas/y/respuestas/PreguntasYRespuestas.java
@@ -1,11 +1,5 @@
 package preguntas.y.respuestas;
 
-import vista.configuracion;
-import vista.inicio;
-import vista.pantalla_de_juego;
-import vista.pantalla_final;
-import vista.puntuaciones;
-
 /**
  *
  * @author Daniel Alzate
@@ -16,14 +10,8 @@ public class PreguntasYRespuestas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//        inicio i = new inicio();
-//        i.setVisible(true);
-//        configuracion c = new configuracion();
-//        c.setVisible(true);
-//        puntuaciones p = new puntuaciones();
-//        p.setVisible(true);
-//        pantalla_de_juego.main(args);
-        pantalla_final.main(args);
+        System.out.println("hola");
+        System.out.println("");
     }
     
 }

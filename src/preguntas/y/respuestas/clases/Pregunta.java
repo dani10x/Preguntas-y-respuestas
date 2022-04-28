b1b53fd56153bca43e198fe53099551373384b03
Daniel Alzate <104457674+dani10x@users.noreply.github.com>
2022-04-27 19:52:10 -0500

    Revert "Revert "implementacion de las clases faltantes""
    This reverts commit 856144cc74b59bf54d0013eaa1fc475bcbd06c3a.

--- src/preguntas/y/respuestas/clases/Pregunta.java
+++ src/preguntas/y/respuestas/clases/Pregunta.java
@@ -7,53 +7,13 @@ import java.util.ArrayList;
  * @author Daniel Alzate
  */
 public final class Pregunta {
-    private Categoria categoria;
     private String pregunta;
-    private ArrayList<Respuesta> respuestas;
+    private ArrayList respuestas;
 
-    public Pregunta(Categoria categoria, String pregunta, ArrayList<Respuesta> respuestas) {
-        this.categoria=categoria;
+    public Pregunta(String pregunta, ArrayList respuestas) {
         this.pregunta = pregunta;
-        this.respuestas = respuestas;
-    }
-
-    public Categoria getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(Categoria categoria) {
-        this.categoria = categoria;
-    }
-
-    public String getPregunta() {
-        return pregunta;
-    }
-
-    public void setPregunta(String pregunta) {
-        this.pregunta = pregunta;
-    }
-
-    public ArrayList getRespuestas() {
-        return respuestas;
-    }
-
-    public void setRespuestas(ArrayList respuestas) {
         this.respuestas = respuestas;
     }
     
-    public void mezclarRespuestas(){
-        if(respuestas!=null){
-            for(int i=0; i<4; i++){
-                int random1=(int) Math.floor(Math.random()*(4)+1);
-                int random2=0;
-                do{
-                    random2=(int) Math.floor(Math.random()*(4)+1);
-                }while(random2==random1);
-                
-                Respuesta aux = new Respuesta(respuestas.get(random2));
-                respuestas.set(random2, respuestas.get(random1));
-                respuestas.set(random1, aux);
-            }
-        }
-    }
+    
 }

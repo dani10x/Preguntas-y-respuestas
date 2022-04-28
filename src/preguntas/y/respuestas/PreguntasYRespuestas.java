856144cc74b59bf54d0013eaa1fc475bcbd06c3a
Daniel Alzate <104457674+dani10x@users.noreply.github.com>
2022-04-27 19:50:56 -0500

    Revert "implementacion de las clases faltantes"
    This reverts commit b3d9351a4261a1347e882d9edb078bbd950a9803.

--- src/preguntas/y/respuestas/PreguntasYRespuestas.java
+++ src/preguntas/y/respuestas/PreguntasYRespuestas.java
@@ -1,5 +1,11 @@
 package preguntas.y.respuestas;
 
+import vista.configuracion;
+import vista.inicio;
+import vista.pantalla_de_juego;
+import vista.pantalla_final;
+import vista.puntuaciones;
+
 /**
  *
  * @author Daniel Alzate
@@ -10,8 +16,14 @@ public class PreguntasYRespuestas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola");
-        System.out.println("");
+//        inicio i = new inicio();
+//        i.setVisible(true);
+//        configuracion c = new configuracion();
+//        c.setVisible(true);
+//        puntuaciones p = new puntuaciones();
+//        p.setVisible(true);
+//        pantalla_de_juego.main(args);
+        pantalla_final.main(args);
     }
     
 }
